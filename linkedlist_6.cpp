db4aa251d524f2c41cae0ce16817533ba2fbb072
durga0000 <81485960+durga0000@users.noreply.github.com>
2021-04-05 16:40:27 +0530

    Add files via upload

---
+++ linkedlist_6.cpp
@@ -0,0 +1,20 @@
+ lass Solution {
+public:
+    bool hasCycle(ListNode *head)
+    {
+        ListNode* slow=head;
+        ListNode* fast=head;
+       
+        while( fast!=NULL && fast->next!=NULL)
+        {
+          
+            fast=fast->next->next;
+              slow=slow->next;
+            if(fast==slow)
+            {
+                return 1;
+            }
+        }
+        return 0;
+    }
+};
