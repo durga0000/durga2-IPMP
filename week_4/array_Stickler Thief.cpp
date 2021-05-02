a0eef38c366edc3b7da95aee1d00a48385f82a48
durga0000 <81485960+durga0000@users.noreply.github.com>
2021-05-02 21:26:05 +0530

    Add files via upload

---
+++ week_4/array_Stickler Thief.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    public:
+    void r(vector<int>& nums,int low,int high)
+    {
+       if(low<high)            
+        {
+            swap(nums[low],nums[high]);
+            low++;
+            high--;
+        }
+    }
+public:
+    void rotate(vector<int>& nums, int k)
+    {
+        //first we reverse first part 
+        //next we reverse last k
+        //next we reverse total array 
+        
+        
+        int n=nums.size();
+      
+        k=k%n;// question is we have to rotate array k times if k is greater than n . we use % so k will necome always 1
+       r(nums,0,n-k-1);
+        r(nums,n-k,n-1);
+        r(nums,0,n-1);
+    }
+};g
